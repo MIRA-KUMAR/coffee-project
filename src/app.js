423a00c8d1978134576390b9e68a665db1ab14a8
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-18 16:07:33 -0700

    Committing routing code in server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const readCoffeeInfo = require('./CoffeeInformation/read')
 const app = express();
+
+
+// middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routing
+app.get('/coffeeInformation', readCoffeeInfo);
 
 app.listen('3000', function() {                 // Callback function -- after listening to port 3000, function() is executed
     console.log('Server started');
 });
 
-app.use('/', function(req, res) {
-    res.send('Hello World');
-})
+// app.use('/', function(req, res) {
+//     res.send('Hello World');
+// })
+
