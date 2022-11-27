93af30ebc50bc7a2a15bdc71e80e3202693fe02d
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-27 00:27:17 -0700

    committing index route changes

--- src/schemas/coffeeinformation/index.js
+++ src/schemas/coffeeinformation/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 const read = require('./read'); 
 const readById = require('./readOne');
 const update = require('./update');
+const create = require('./create');
+const del = require('./delete');
+
 const router = express.Router();
 
 router.get('/', read);
 router.get('/:id', readById);
 router.put('/:id', update);
+router.post('/', create);
+router.delete('/:id', del);
 
 module.exports = router;
