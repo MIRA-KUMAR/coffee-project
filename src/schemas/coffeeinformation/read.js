74c34071191f76c59d71e4cc4c43c939c4e12563
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-26 19:54:35 -0700

    cimmitting read route

--- src/schemas/coffeeinformation/read.js
+++ src/schemas/coffeeinformation/read.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
+const joi = require('joi');
 
+const schema = joi.object({
+    page: joi.number().integer().min(1).default(1),
+    limit: joi.number().integer().min(1).default(5),
+})
 module.exports = async (req, res, next) => {
-    const CoffeeInformationModel = mongoose.model('CoffeeInformation');
-    const data = await CoffeeInformationModel.find();
-    // console.log(data);
-    return res.send(data);
+    
+    try {
+        const {error, value} = schema.validate(req.query); 
+    
+        if(error){
+            return res.send(error);
+        }
+        const CoffeeInformationModel = mongoose.model('CoffeeInformation');
+
+        const {page, limit} = value;
+        const coffeeinfo = await CoffeeInformationModel.find().skip((page-1)*limit).limit(limit);
+        const count = await CoffeeInformationModel.countDocuments();
+        const totalPages = Math.ceil(count/limit);
+        
+        return res.status(200).send({
+            data: coffeeinfo,
+            paging: {
+                total: count,
+                page: page,
+                pages: totalPages,
+            },
+        });
+
+    }
+
+    catch(err) {
+        return res.status(500).send(err);
+    }
 };
