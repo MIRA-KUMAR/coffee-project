7aadfc6a74199d19ffc47734b495ea5f01f33fbe
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-26 23:57:19 -0700

    Committing changes to index routes

--- src/schemas/coffeeinformation/index.js
+++ src/schemas/coffeeinformation/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const read = require('./read');
-const readById = require('./readOne')
+const read = require('./read'); 
+const readById = require('./readOne');
+const update = require('./update');
 const router = express.Router();
 
 router.get('/', read);
 router.get('/:id', readById);
+router.put('/:id', update);
 
 module.exports = router;
