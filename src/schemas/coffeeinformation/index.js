602db6386f6c91c62bcfa945487923477a56c33e
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-26 19:54:47 -0700

    commit index route

--- src/schemas/coffeeinformation/index.js
+++ src/schemas/coffeeinformation/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const read = require('./read');
-// const readById = require('./readById')
+const readById = require('./readOne')
 const router = express.Router();
 
 router.get('/', read);
-// router.get('/:id', readById);
+router.get('/:id', readById);
 
 module.exports = router;
