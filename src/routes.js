c13324656623aacd7cef3e55e557712daba0e43f
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-11-27 12:17:07 -0700

    changes to routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const coffeeinformation = require('./schemas/coffeeinformation');
-// const coffeetype = require('./schemas/coffeetype');
-// const coffeeseller = require('./schemas/coffeeseller');
+const coffeetype = require('./schemas/coffeetype');
+const coffeeseller = require('./schemas/coffeeseller');
 // const coffeename = require('./schemas/coffeename');
 
 const router = express.Router();
 
 router.use('/coffeeinformation', coffeeinformation);
-// router.use('/coffeetype', coffeetype);
+router.use('/coffeetype', coffeetype);
 // router.use('/coffeename', coffeename);
-// router.use('/coffeeseller', coffeeseller);
+router.use('/coffeeseller', coffeeseller);
 
 module.exports = router;
